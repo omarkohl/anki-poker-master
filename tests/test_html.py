51168af7bc5cce0b8ea98ad5cad6617746164ada
Omar Kohl <omarkohl@posteo.net>
2024-02-22 11:07:03 +0100

    add .marked class and JS to toggle cells being marked
    This is helpful during learning to better remember what I'm doing.

--- tests/test_html.py
+++ tests/test_html.py
@@ -21,7 +21,7 @@ def test_default_extra_css(pytestconfig, golden_dir):
     or change any colors.
     """
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -35,7 +35,7 @@ def test_default_extra_css(pytestconfig, golden_dir):
 
 def test_custom_fold_color_css(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -52,6 +52,7 @@ def test_custom_fold_color_css(pytestconfig, golden_dir):
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_full()
     html += scenario.html_legend()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
@@ -61,7 +62,7 @@ def test_custom_range_css(pytestconfig, golden_dir):
     Test that the custom range is added to the css with one of the predefined colors.
     """
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3s+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -74,6 +75,7 @@ def test_custom_range_css(pytestconfig, golden_dir):
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_full()
     html += scenario.html_legend()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
@@ -83,7 +85,7 @@ def test_custom_range_with_custom_color_css(pytestconfig, golden_dir):
     Test that the custom range is added to the css with the color passed in.
     """
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3s+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -100,13 +102,14 @@ def test_custom_range_with_custom_color_css(pytestconfig, golden_dir):
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_full()
     html += scenario.html_legend()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
 
 def test_legend_html(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -118,13 +121,14 @@ def test_legend_html(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_legend()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
 
 def test_full_html(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -136,13 +140,14 @@ def test_full_html(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_full()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
 
 def test_blank_html(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -154,13 +159,14 @@ def test_blank_html(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_blank()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
 
 def test_top_left_quadrant_blank_html(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -172,13 +178,14 @@ def test_top_left_quadrant_blank_html(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_top_left_quadrant_blank()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
 
 def test_top_right_quadrant_blank_html(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -190,13 +197,14 @@ def test_top_right_quadrant_blank_html(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_top_right_quadrant_blank()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
 
 def test_bottom_left_quadrant_blank_html(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -208,13 +216,14 @@ def test_bottom_left_quadrant_blank_html(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_bottom_left_quadrant_blank()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
 
 def test_bottom_right_quadrant_blank_html(pytestconfig, golden_dir):
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -226,6 +235,7 @@ def test_bottom_right_quadrant_blank_html(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_bottom_right_quadrant_blank()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
@@ -235,7 +245,7 @@ def test_top_right_quadrant_blank_html_with_custom_ranges(pytestconfig, golden_d
     The purpose of this test is to verify that custom CSS also works with blank.
     """
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A3+, K3+, Q3+, J3+, T3+, 93+, 83+, 73+'),
         "Raise": Range('AA, KK, QQ, JJ, TT, 99, 88, 77'),
@@ -251,6 +261,7 @@ def test_top_right_quadrant_blank_html_with_custom_ranges(pytestconfig, golden_d
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_top_right_quadrant_blank()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
@@ -260,7 +271,7 @@ def test_overlapping_ranges(pytestconfig, golden_dir):
     Verify that overlapping ranges are overwritten in alphabetical order.
     """
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {
         "Call": Range('A8s-'),
         "AAA": Range('AKs'),
@@ -273,6 +284,7 @@ def test_overlapping_ranges(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_full()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
 
@@ -282,7 +294,7 @@ def test_default_is_fold(pytestconfig, golden_dir):
     Verify that if no ranges are passed in, the default is to fold.
     """
     from anki_poker_generator import PreflopScenario
-    from anki_poker_generator.const import DEFAULT_CSS
+    from anki_poker_generator.const import DEFAULT_CSS, DEFAULT_JS
     action_ranges = {}
     config = {}
     scenario = PreflopScenario(action_ranges, "CO", "Opening", "Cash 100BB 6P", config)
@@ -290,5 +302,6 @@ def test_default_is_fold(pytestconfig, golden_dir):
     html += '<style>\n' + textwrap.indent(DEFAULT_CSS, 4*' ') + '</style>\n'
     html += '<style>\n'+textwrap.indent(scenario.extra_css(), 4*' ')+'</style>\n'
     html += scenario.html_full()
+    html += '<script>\n' + textwrap.indent(DEFAULT_JS, 4*' ') + '</script>\n'
     golden_html_file = os.path.join(golden_dir, "file.html")
     compare_or_update_golden(pytestconfig, golden_html_file, html)
