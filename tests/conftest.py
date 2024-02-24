1df09cfc8cabeee0f455686c4f02e487b03ebc3d
Omar Kohl <omarkohl@posteo.net>
2024-02-24 18:46:04 +0100

    tests: move 'golden' files to subdirectory to avoid conflicts
    e.g. if another test module wants to create golden files

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,12 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--update-golden", action="store_true", help="Update the golden files", default=False)
+    parser.addoption(
+        "--update-golden",
+        action="store_true",
+        help="Update the golden files",
+        default=False,
+    )
 
 
 @pytest.fixture
@@ -11,6 +16,7 @@ def golden_dir(request):
     golden_dir_path = os.path.join(
         os.path.dirname(__file__),
         "golden",
+        request.node.module.__name__.split(".")[-1],
         request.node.function.__name__,
     )
     if not os.path.exists(golden_dir_path):
