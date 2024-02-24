cea25293df606e7634ddaa752eb7a66d8fffdf8b
Omar Kohl <omarkohl@posteo.net>
2024-02-24 17:58:26 +0100

    tests: some more tests for CLI params

--- tests/test_main.py
+++ tests/test_main.py
@@ -25,3 +25,72 @@ def test_version(capsys):
     captured = capsys.readouterr()
     assert captured.out.startswith("AnkiPokerMaster")
     assert len(captured.out) > len("AnkiPokerMaster\n")
+
+
+def test_example_config(capsys, tmp_path):
+    from anki_poker_master.cli import main_with_args, EXAMPLE_CONFIG_FILE
+
+    config_file = tmp_path / "config.yml"
+    main_with_args(["-c", str(config_file), "-e"])
+    captured = capsys.readouterr()
+    assert "Example configuration file written to" in captured.out
+    assert config_file.exists()
+    with open(config_file, "r") as f:
+        assert f.read().strip() == EXAMPLE_CONFIG_FILE.strip()
+
+
+def test_example_scenarios(capsys, tmp_path):
+    from anki_poker_master.cli import main_with_args, EXAMPLE_SCENARIO_FILE
+
+    scenarios_file = tmp_path / "scenarios.yml"
+    main_with_args(["-s", str(scenarios_file), "-e"])
+    captured = capsys.readouterr()
+    assert "Example scenarios file written to" in captured.out
+    assert scenarios_file.exists()
+    with open(scenarios_file, "r") as f:
+        assert f.read().strip() == EXAMPLE_SCENARIO_FILE.strip()
+
+
+def test_example_config_and_scenarios(capsys, tmp_path):
+    from anki_poker_master.cli import (
+        main_with_args,
+        EXAMPLE_CONFIG_FILE,
+        EXAMPLE_SCENARIO_FILE,
+    )
+
+    config_file = tmp_path / "config.yml"
+    scenarios_file = tmp_path / "scenarios.yml"
+    main_with_args(["-c", str(config_file), "-s", str(scenarios_file), "-e"])
+    captured = capsys.readouterr()
+    assert "Example configuration file written to" in captured.out
+    assert "Example scenarios file written to" in captured.out
+    assert config_file.exists()
+    assert scenarios_file.exists()
+    with open(config_file, "r") as f:
+        assert f.read().strip() == EXAMPLE_CONFIG_FILE.strip()
+    with open(scenarios_file, "r") as f:
+        assert f.read().strip() == EXAMPLE_SCENARIO_FILE.strip()
+
+
+def test_example_config_only_if_doesnt_exist(capsys, tmp_path):
+    from anki_poker_master.cli import main_with_args, EXAMPLE_CONFIG_FILE
+
+    config_file = tmp_path / "config.yml"
+    config_file.write_text("existing file")
+    with pytest.raises(SystemExit) as e:
+        main_with_args(["-c", str(config_file), "-e"])
+    captured = capsys.readouterr()
+    assert "The file" in captured.out
+    assert "already exists" in captured.out
+
+
+def test_example_scenarios_only_if_doesnt_exist(capsys, tmp_path):
+    from anki_poker_master.cli import main_with_args, EXAMPLE_SCENARIO_FILE
+
+    scenarios_file = tmp_path / "scenarios.yml"
+    scenarios_file.write_text("existing file")
+    with pytest.raises(SystemExit) as e:
+        main_with_args(["-s", str(scenarios_file), "-e"])
+    captured = capsys.readouterr()
+    assert "The file" in captured.out
+    assert "already exists" in captured.out
