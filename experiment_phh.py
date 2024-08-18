6a00931fe18a3940214f4b23df1703705eaf30dd
Omar Kohl <omarkohl@posteo.net>
2024-08-18 19:40:42 +0200

    dev: add example.phh file

--- experiment_phh.py
+++ experiment_phh.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# poetry run python experiment_phh.py /some/path/example.phh
+# poetry run python experiment_phh.py ./example.phh
 
 import sys
 from anki_poker_master.hand import parse_phh
