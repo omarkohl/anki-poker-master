cff74348c0f162c57e0f84513d4c41fe463835e8
Omar Kohl <omarkohl@posteo.net>
2024-08-25 18:04:15 +0200

    refactor: use a new class _StateMachine in parse_phh()

--- anki_poker_master/parser/phh.py
+++ anki_poker_master/parser/phh.py
@@ -1,7 +1,8 @@
 import enum
 import tomllib
-from typing import Dict, Any, List, Optional
+from typing import Dict, Any, List, Optional, Generator, Tuple
 
+import pokerkit
 import schema
 from pokerkit import HandHistory, HoleDealing, Card
 
@@ -13,6 +14,80 @@ class _GameState(enum.Enum):
     SETUP = enum.auto()
     END_SETUP = enum.auto()
     PREFLOP = enum.auto()
+    DONE = enum.auto()
+
+
+class _StateMachine:
+    _machine_state: _GameState
+    _pk_operation_iterator: Generator[Tuple[pokerkit.State, pokerkit.Operation], None, None]
+    _pk_current_state: pokerkit.State
+    _pk_current_operation: pokerkit.Operation
+    _nr_players_dealt: int
+    _hand: Hand
+
+    def __init__(self, hh: pokerkit.HandHistory, custom_fields: Dict[str, Any]):
+        self._machine_state = _GameState.SETUP
+        self._pk_operation_iterator = self._create_pk_operation_iterator(hh)
+        self._nr_players_dealt = 0
+        self._custom_fields = custom_fields
+        self._hand = Hand()
+        player_count = hh.create_state().player_count
+        for i in range(player_count):
+            name = f'p{i + 1}'
+            if hh.players:
+                name = hh.players[i]
+            is_dealer = (i == player_count - 1)
+            self._hand.players.append(Player(name, is_dealer, False))
+
+    def get_hand(self) -> Hand:
+        while self._machine_state != _GameState.DONE:
+            # why do I need the current_state, only for player_count?
+            try:
+                self._pk_current_state, self._pk_current_operation = next(self._pk_operation_iterator)
+            except StopIteration:
+                self._machine_state = _GameState.DONE
+            if self._machine_state == _GameState.SETUP:
+                self._state_setup()
+            elif self._machine_state == _GameState.END_SETUP:
+                pass
+            elif self._machine_state == _GameState.PREFLOP:
+                pass
+        return self._hand
+
+    @staticmethod
+    def _create_pk_operation_iterator(hh: pokerkit.HandHistory) -> (
+            Generator)[Tuple[pokerkit.State, pokerkit.Operation], None, None]:
+        index = 0
+        for s in hh:
+            while index < len(s.operations):
+                yield s, s.operations[index]
+                index += 1
+
+    def _state_setup(self):
+        if isinstance(self._pk_current_operation, HoleDealing):
+            self._nr_players_dealt += 1
+            if self._nr_players_dealt == self._pk_current_state.player_count:
+                hero_index, self._hand.hero_cards = _get_hero(self._pk_current_state.hole_cards,
+                                                              self._custom_fields.get("_apm_hero", None))
+                self._hand.players[hero_index].is_hero = True
+                blinds = sum(self._pk_current_state.blinds_or_straddles)
+                pot_amounts = list(self._pk_current_state.pot_amounts)
+                if not pot_amounts:
+                    pot_amounts = [0]
+                pot_amounts[0] += blinds
+                self._hand.streets.append(
+                    Street(
+                        "Preflop",
+                        [],
+                        pot_amounts,
+                        [True for _ in range(self._pk_current_state.player_count)],
+                        self._pk_current_state.stacks.copy(),
+                        2,
+                        [[] for _ in range(self._pk_current_state.player_count)],
+                    )
+                )
+                self._machine_state = _GameState.PREFLOP
+
 
 def parse_phh(content: str) -> Hand:
     if not content:
@@ -30,53 +105,9 @@ def parse_phh(content: str) -> Hand:
         # TODO Validate whether other variants work with little additional effort, but for now focus on NLHE
         raise ValidationError(f"the variant '{hh.variant}' is not supported")
 
-    pk_state = hh.create_state()
-
-    custom_fields = _get_and_validate_custom_fields(content, pk_state.player_count)
-
-    my_hand = Hand()
-    for i in range(pk_state.player_count):
-        name = f'p{i + 1}'
-        if hh.players:
-            name = hh.players[i]
-        is_dealer = (i == pk_state.player_count - 1)
-        my_hand.players.append(Player(name, is_dealer, False))
-
-    state = _GameState.SETUP
-    dealing_counter = 0
-    operation_index = 0
-    for pk_state in hh:
-        while operation_index < len(pk_state.operations):
-            operation = pk_state.operations[operation_index]
-            operation_index += 1
-            if state is _GameState.SETUP:
-                if isinstance(operation, HoleDealing):
-                    dealing_counter += 1
-                    if dealing_counter == pk_state.player_count:
-                        state = _GameState.END_SETUP
-            if state is _GameState.END_SETUP:
-                hero_index, my_hand.hero_cards = _get_hero(pk_state.hole_cards, custom_fields.get("_apm_hero", None))
-                my_hand.players[hero_index].is_hero = True
-                blinds = sum(pk_state.blinds_or_straddles)
-                pot_amounts = list(pk_state.pot_amounts)
-                if not pot_amounts:
-                    pot_amounts = [0]
-                pot_amounts[0] += blinds
-                my_hand.streets.append(
-                    Street(
-                        "Preflop",
-                        [],
-                        pot_amounts,
-                        [True for _ in range(pk_state.player_count)],
-                        pk_state.stacks.copy(),
-                        2,
-                        [[] for _ in range(pk_state.player_count)],
-                    )
-                )
-                state = _GameState.PREFLOP
-                continue
-
-    return my_hand
+    custom_fields = _get_and_validate_custom_fields(content, hh.create_state().player_count)
+    state_machine = _StateMachine(hh, custom_fields)
+    return state_machine.get_hand()
 
 
 def _get_and_validate_custom_fields(content: str, player_count: int) -> Dict[str, Any]:
