4eaa8831a7ced43f7083d8a0da325348f810be1e
Omar Kohl <omarkohl@posteo.net>
2024-08-25 18:03:57 +0200

    dev: continue experimenting with phh parsing

--- anki_poker_master/hand.py
+++ anki_poker_master/hand.py
@@ -1,7 +1,59 @@
+import pprint
+from numbers import Number
 from pathlib import Path
+from typing import List, Optional
+
 from pokerkit import HandHistory
 from pokerkit import HoleDealing, BoardDealing, CheckingOrCalling, CompletionBettingOrRaisingTo, Folding
 from tomllib import loads
+
+class Street:
+    name: str
+    board: List[str]
+    initial_pots: List[Number]
+    initial_stacks: List[Number]
+    initial_players: List[bool]
+    first_player: int
+    # will start with 'first_player'
+    actions: List[List[str]]
+
+    def __init__(self, name, board, initial_pots, initial_players, initial_stacks, first_player, actions):
+        self.name = name
+        self.board = board
+        self.initial_pots = initial_pots
+        self.initial_players = initial_players
+        self.initial_stacks = initial_stacks
+        self.first_player = first_player
+        self.actions = actions
+
+class Player:
+    name: str
+    is_dealer: bool
+    is_hero: bool
+
+    def __init__(self, name: str, is_dealer: bool, is_hero: bool):
+        self.name = name
+        self.is_dealer = is_dealer
+        self.is_hero = is_hero
+
+    def __repr__(self):
+        return f'Player("{self.name}", is_dealer={self.is_dealer}, is_hero={self.is_hero})'
+
+
+class Hand:
+    # last one is the dealer
+    players: List[Player]
+    hero_cards: List[str]
+    streets: List[Street]
+
+    def __init__(self):
+        self.players: List[Player] = []
+        self.hero_cards: List[str] = ['', '']
+        self.streets: List[Street] = []
+
+    def __str__(self):
+        return f'{self.players} {self.hero_cards} {self.streets}'
+
 
 def parse_phh(path: Path) -> None:
     with open(path, "rb") as file:
@@ -14,6 +66,10 @@ def parse_phh(path: Path) -> None:
     if "_apm_answers" in raw_file:
         answers = raw_file["_apm_answers"]
 
+    hero_index: Optional[int] = None
+    if "_apm_hero" in raw_file:
+        hero_index = int(raw_file["_apm_hero"])
+
     game = hh.create_game()
     state = hh.create_state()
     print()
@@ -22,8 +78,17 @@ def parse_phh(path: Path) -> None:
     print("---------")
     print()
 
-    # starting with 1
-    HERO = 7
+    my_hand = Hand()
+
+    for i in range(state.player_count):
+        name = f'p{i+1}'
+        if hh.players:
+            name = hh.players[i]
+        is_dealer = (i == state.player_count - 1)
+        my_hand.players.append(Player(name, is_dealer, False))
+
+    pprint.pp(vars(my_hand))
+    print()
 
     question_counter = 0
 
@@ -31,6 +96,7 @@ def parse_phh(path: Path) -> None:
     hero_cards = ['', '']
     my_status = "SETUP"
     hole_dealing_counter = 0
+    there_has_been_a_bet = False
     # TODO modify again to only iterate over states since I don't need the actions
     for state, _ in hh.iter_state_actions():  # I'm assuming number of states always matches number of actions
         while operation_index < len(state.operations):
@@ -42,40 +108,54 @@ def parse_phh(path: Path) -> None:
             if my_status == "DEALING":
                 if isinstance(operation, HoleDealing):
                     hole_dealing_counter += 1
-                    if operation.player_index == HERO - 1:
-                        for i, card in enumerate(operation.cards):
-                            if not card.unknown_status:
-                                hero_cards[i] = repr(
+                    if all(not c.unknown_status for c in operation.cards):
+                        if hero_index is None or hero_index - 1 == operation.player_index:
+                            my_hand.players[operation.player_index].is_hero = True
+                            for i, card in enumerate(operation.cards):
+                                my_hand.hero_cards[i] = repr(
                                     card,
                                 )
                     if hole_dealing_counter == state.player_count:
                         my_status = "START_PREFLOP"
             if my_status == "START_PREFLOP":
-                print("Hero is:", hh.players[HERO - 1])
-                print("and has cards:", hero_cards)
                 blinds = sum(state.blinds_or_straddles)
                 pot_amounts = list(state.pot_amounts)
                 if not pot_amounts:
                     pot_amounts = [0]
                 pot_amounts[0] += blinds
-                print("Initial pot is:", pot_amounts)
-                print("Players are:", hh.players)
-                print("With stacks:", state.stacks)
+                my_hand.streets.append(
+                    Street(
+                        "Preflop",
+                        [],
+                        pot_amounts,
+                        [True for _ in range(state.player_count)],
+                        state.stacks.copy(),
+                        2,
+                        [[] for _ in range(state.player_count)],
+                    )
+                )
+                pprint.pp(vars(my_hand))
+                pprint.pp(vars(my_hand.streets[0]))
                 print()
                 my_status = "PREFLOP"
+                there_has_been_a_bet = False
                 continue
             if my_status == "PREFLOP":
                 if isinstance(operation, BoardDealing):
                     my_status = "START_FLOP"
                 elif isinstance(operation, CheckingOrCalling):
+                    my_hand.streets[0].actions[operation.player_index].append("C" if there_has_been_a_bet else "X")
                     print(hh.players[operation.player_index])
                     print("C")
                     print()
                 elif isinstance(operation, CompletionBettingOrRaisingTo):
+                    my_hand.streets[0].actions[operation.player_index].append(f'{"R" if there_has_been_a_bet else "B"} {operation.amount}')
+                    there_has_been_a_bet = True
                     print(hh.players[operation.player_index])
                     print("R", operation.amount)  # why not operation.amount?
                     print()
                 elif isinstance(operation, Folding):
+                    my_hand.streets[0].actions[operation.player_index].append("F")
                     print(hh.players[operation.player_index])
                     print("F")
                     print()
@@ -89,6 +169,10 @@ def parse_phh(path: Path) -> None:
                     question_counter += 1
                     print()
             if my_status == "START_FLOP":
+                pprint.pp(vars(my_hand))
+                pprint.pp(vars(my_hand.streets[0]))
+                print()
+
                 print("Starting the flop with cards:", operation.cards)
                 print("And pots:", list(state.pot_amounts))
                 active_players = []
@@ -99,20 +183,40 @@ def parse_phh(path: Path) -> None:
                 print("With stacks:", state.stacks)
                 print("Active players:", ", ".join(active_players))
                 print()
+                my_hand.streets.append(
+                    Street(
+                        "Flop",
+                        [repr(c) for c in operation.cards],
+                        list(state.pot_amounts),
+                        state.statuses.copy(),
+                        state.stacks.copy(),
+                        0,
+                        [[] for _ in range(state.player_count)],
+                    )
+                )
+                pprint.pp(vars(my_hand))
+                pprint.pp(vars(my_hand.streets[1]))
+                print()
+
+                there_has_been_a_bet = False
                 my_status = "FLOP"
                 continue
             if my_status == "FLOP":
                 if isinstance(operation, BoardDealing):
                     my_status = "START_TURN"
                 elif isinstance(operation, CheckingOrCalling):
+                    my_hand.streets[1].actions[operation.player_index].append("C" if there_has_been_a_bet else "X")
                     print(hh.players[operation.player_index])
                     print("C")
                     print()
                 elif isinstance(operation, CompletionBettingOrRaisingTo):
+                    my_hand.streets[1].actions[operation.player_index].append(f'{"R" if there_has_been_a_bet else "B"} {operation.amount}')
+                    there_has_been_a_bet = True
                     print(hh.players[operation.player_index])
                     print("R", operation.amount)  # why not operation.amount?
                     print()
                 elif isinstance(operation, Folding):
+                    my_hand.streets[1].actions[operation.player_index].append("F")
                     print(hh.players[operation.player_index])
                     print("F")
                     print()
@@ -126,6 +230,10 @@ def parse_phh(path: Path) -> None:
                     question_counter += 1
                     print()
             if my_status == "START_TURN":
+                pprint.pp(vars(my_hand))
+                pprint.pp(vars(my_hand.streets[1]))
+                print()
+
                 print("Starting the turn with cards:", operation.cards)
                 print("And pots:", list(state.pot_amounts))
                 active_players = []
@@ -136,6 +244,7 @@ def parse_phh(path: Path) -> None:
                 print("With stacks:", state.stacks)
                 print("Active players:", ", ".join(active_players))
                 print()
+                there_has_been_a_bet = False
                 my_status = "TURN"
                 continue
             if my_status == "TURN":
@@ -173,6 +282,7 @@ def parse_phh(path: Path) -> None:
                 print("With stacks:", state.stacks)
                 print("Active players:", ", ".join(active_players))
                 print()
+                there_has_been_a_bet = False
                 my_status = "RIVER"
                 continue
             if my_status == "RIVER":
