5146214d49e6771d0088fbecf9ba13d0a4ae154e
Omar Kohl <omarkohl@posteo.net>
2024-08-25 18:04:01 +0200

    feat: continue with phh parsing

--- anki_poker_master/parser/phh.py
+++ anki_poker_master/parser/phh.py
@@ -1,15 +1,12 @@
 from pokerkit import HandHistory
 
 from anki_poker_master.model import ValidationError
-from anki_poker_master.model.hand import Hand
+from anki_poker_master.model.hand import Hand, Player
 
 
 def parse_phh(content: str) -> Hand:
     if not content:
         raise ValidationError("Invalid PHH (empty)")
-
-    my_hand = Hand()
-
     try:
         hh = HandHistory.loads(content)
     except Exception as e:
@@ -18,4 +15,14 @@ def parse_phh(content: str) -> Hand:
         else:
             content_for_err = content
         raise ValidationError(f'Error parsing PHH with content:\n{content_for_err}') from e
+
+    state = hh.create_state()
+
+    my_hand = Hand()
+    for i in range(state.player_count):
+        name = f'p{i + 1}'
+        if hh.players:
+            name = hh.players[i]
+        is_dealer = (i == state.player_count - 1)
+        my_hand.players.append(Player(name, is_dealer, False))
     return my_hand
--- tests/parser/test_phh.py
+++ tests/parser/test_phh.py
@@ -6,9 +6,30 @@ from anki_poker_master.model import ValidationError
 def test_phh_parser():
     from anki_poker_master.parser.phh import parse_phh
 
-    content = ""
+    content = """variant = "NT"
+antes = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+blinds_or_straddles = [2, 4, 0, 0, 0, 0, 0, 0, 0]
+min_bet = 2
+starting_stacks = [110, 420, 450, 340, 280, 400, 570, 100, 990]
+actions = [
+  # Pre-flop
+  "d dh p1 ????",
+  "d dh p2 ????",
+  "d dh p3 ????",
+  "d dh p4 ????",
+  "d dh p5 ????",
+  "d dh p6 ????",
+  "d dh p7 Th8c",
+  "d dh p8 ????",
+  "d dh p9 ????",
+]
+"""
     hand = parse_phh(content)
     assert hand is not None
+    assert len(hand.players) == 9
+    assert hand.players[1].name == "p2"
+    assert hand.players[8].is_dealer
+
 
 
 def test_phh_parser_emtpy_file():
