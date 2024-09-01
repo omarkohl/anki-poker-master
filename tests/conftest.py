9ab506970a210c82c3e1a9d0dfda36b64e67725b
Omar Kohl <omarkohl@posteo.net>
2024-09-01 21:43:48 +0200

    tests: document testdata_dir and golden_dir test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,26 @@ def pytest_addoption(parser):
 
 @pytest.fixture
 def golden_dir(request):
+    """
+    Return the path to the golden directory for the test currently being executed. The directory
+    will be created if it does not exist. A golden file is a file that contains the output that
+    is expected from some execution so the output can be compared to that golden file. It's
+    more convenient than embedding large amounts of text or data within the test itself.
+    Golden directory just means a directory containing golden files.
+
+    The location of the golden directory will be:
+      - Inside the current test package a directory "golden"
+      - Within that directory a directory named like the current test module
+      - Within that directory a directory named like the current test function
+
+    For example the golden directory for the test function "test_parser_player_names" contained
+    in tests/parser/test_phh.py will be tests/parser/golden/test_phh/test_parser_player_names/
+    The test can access any file within that directory. Those files must be created.
+
+    Note that the test itself is expected to overwrite the content of the files when tests are
+    invoked with --update-golden as a convenience for the developer (instead of having to manually
+    update all golden files).
+    """
     modules = request.node.module.__name__.split(".")[1:]  # excluding initial "tests"
     golden_dir_path = os.path.join(
         os.path.dirname(__file__),
@@ -30,6 +50,19 @@ def golden_dir(request):
 
 @pytest.fixture
 def testdata_dir(request):
+    """
+    Return the path to the test data directory for any data the test may need. The directory will
+    be created if it does not exist.
+
+    The location of the test data directory will be:
+      - Inside the current test package a directory "testdata"
+      - Within that directory a directory named like the current test module
+      - Within that directory a directory named like the current test function
+
+    For example the test data directory for the test function "test_parser_player_names" contained
+    in tests/parser/test_phh.py will be tests/parser/testdata/test_phh/test_parser_player_names/
+    The test can access any file within that directory. Those files must be created.
+    """
     modules = request.node.module.__name__.split(".")[1:]  # excluding initial "tests"
     testdata_dir_path = os.path.join(
         os.path.dirname(__file__),
