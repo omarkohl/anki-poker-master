14e2c7ad63c8e0181dc548610d59b7e96f039eec
Omar Kohl <omarkohl@posteo.net>
2024-09-28 18:51:42 +0200

    dev: add minimal skeleton for Anki presenter

---
+++ anki_poker_master/presenter/anki.py
@@ -0,0 +1,6 @@
+from anki_poker_master.model import ValidationError
+from anki_poker_master.model.hand import Hand
+
+
+def get_question(hand: Hand, street_index_for_question: int, question_index: int) -> str:
+    return ""
