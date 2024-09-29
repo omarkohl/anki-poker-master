d52a587b933864184c79b05c981333d360590283
Omar Kohl <omarkohl@posteo.net>
2024-09-29 16:40:35 +0200

    tests: add end to end test for hand history study
    Also add another helper function for golden files, this time to actually
    compare two files instead of comparing some known output with a file.

---
+++ tests/e2e/test_hand.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+from typing import List, Tuple
+import anki
+import anki.importing.apkg
+from anki.collection import ImportAnkiPackageOptions, ImportAnkiPackageRequest
+
+from tests.utils import compare_or_update_golden, compare_or_update_golden_with_path
+
+
+def test_pure_phh_file(pytestconfig, golden_dir, tmp_path):
+    """
+    Verify that when calling anki-poker-master with a "normal" phh file an
+    Anki deck is generated and contains the notes, cards, tags etc. we expect.
+    """
+    from anki_poker_master.cli import main_with_args
+
+    pkg_f = tmp_path / "package.apkg"
+    collection_f = tmp_path / "collection.anki2"
+    phh_f = tmp_path / "hand.phh"
+    phh_f.write_text(
+        """variant = "NT"
+antes = [0, 0, 0]
+blinds_or_straddles = [2, 4, 0]
+min_bet = 2
+starting_stacks = [110, 420, 450]
+actions = [
+    "d dh p1 ????",
+    "d dh p2 Th8c",
+    "d dh p3 ????",
+    "p3 cbr 12",
+    "p1 f",
+    "p2 cc",
+    "d db AhTs8h",
+    "p2 cc",
+    "p3 cbr 20",
+    "p2 cc",
+    "d db 4s",
+    "p2 cc",
+    "p3 cc",
+    "d db Tc",
+    "p2 cbr 388",
+    "p3 f",
+]
+""",
+        encoding="utf-8",
+    )
+
+    collection = anki.collection.Collection(str(collection_f))
+
+    main_with_args([
+        "hand",
+        "-t",
+        "poker",
+        "-o",
+        str(pkg_f),
+        str(phh_f),
+    ])
+
+    collection.import_anki_package(
+        ImportAnkiPackageRequest(
+            package_path=str(pkg_f),
+            options=ImportAnkiPackageOptions(
+                with_scheduling=True,
+                with_deck_configs=True
+            ),
+        )
+    )
+
+    assert collection.tags.all() == ['poker']
+    assert len(collection.decks.all_names_and_ids()) == 3
+    deck_id = collection.decks.id_for_name("AnkiPokerMaster::HandHistory")
+    assert deck_id
+    all_notes_ids = collection.find_notes('')
+    assert len(all_notes_ids) == 5
+
+    for note in [collection.get_note(nid) for nid in all_notes_ids]:
+        assert len(note.fields) == 4
+
+    all_card_ids = collection.find_cards('')
+
+    assert len(all_card_ids) == 5
+
+    all_qa: List[Tuple[str, str]] = []
+    for card in [collection.get_card(cid) for cid in all_card_ids]:
+        all_qa.append((card.question(), card.answer()))
+
+    # assert that there are no duplicate questions, just in case, otherwise
+    # the golden file comparison below will no longer be deterministic
+    assert len(set(qa[0] for qa in all_qa)) == 5
+
+    # sort them to ensure a deterministic order in the golden files
+    all_qa = sorted(all_qa, key=lambda qa: qa[0])
+
+    for i, (q, a) in enumerate(all_qa):
+        compare_or_update_golden(
+            pytestconfig,
+            golden_dir / f"answer_{i:02}.html",
+            a,
+        )
+        compare_or_update_golden(
+            pytestconfig,
+            golden_dir / f"question_{i:02}.html",
+            q,
+        )
+
+    all_media_files = list(Path(collection.media.dir()).rglob("*"))
+
+    expected_media_files = [
+        'apm-card-small-4s.png',
+        'apm-card-small-8c.png',
+        'apm-card-small-8h.png',
+        'apm-card-small-Ah.png',
+        'apm-card-small-Tc.png',
+        'apm-card-small-Th.png',
+        'apm-card-small-Ts.png',
+    ]
+
+    assert sorted([f.name for f in all_media_files]) == sorted(expected_media_files)
+
+    for f in expected_media_files:
+        compare_or_update_golden_with_path(
+            pytestconfig,
+            golden_dir / f,
+            Path(collection.media.dir()) / f,
+        )
--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,8 @@
+import filecmp
+import os
+import shutil
+
+
 def compare_or_update_golden(pytestconfig, golden_file_path, actual_output):
     """
     If the --update-golden command line option is present then update the golden file with the
@@ -15,3 +20,30 @@ def compare_or_update_golden(pytestconfig, golden_file_path, actual_output):
         with open(golden_file_path, "r") as file:
             golden_output = file.read()
         assert actual_output == golden_output
+
+
+def compare_or_update_golden_with_path(pytestconfig, golden_file_path, actual_path):
+    """
+    If the --update-golden command line option is present then overwrite the
+    golden file with the actual_path, otherwise assert that the content of
+    the file is equal to the content of actual_path.
+    """
+    # Tell pytest to not show this function but its caller when displaying a failed test
+    __tracebackhide__ = True
+
+    assert os.path.exists(actual_path), f"actual file {actual_path} does not exist"
+    golden_path_exists = os.path.exists(golden_file_path)
+    if golden_path_exists:
+        files_are_the_same = filecmp.cmp(golden_file_path, actual_path, shallow=True)
+    else:
+        files_are_the_same = False
+
+    # If the --update-golden option was set, update the golden file
+    if pytestconfig.getoption("update_golden"):
+        if not files_are_the_same:
+            shutil.copyfile(actual_path, golden_file_path)
+    else:
+        assert golden_path_exists, f"golden path {golden_file_path} does not exist"
+        assert files_are_the_same, (f"actual path {actual_path} should have "
+                                    f"the same content as golden file "
+                                    f"{golden_file_path} but does not")
